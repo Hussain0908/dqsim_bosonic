f4db6bab4b73b6c68c3efe5ac82347e378ed01b0
Talal Hussain <tsahus123@gmail.com>
2026-06-16 09:47:02 -0600

    feat(types): Including Debug for derive
    An error occurs within src/monolithic/stabilizer/simulator.rs for the 'Instruction' command for 'other'. This errors resolves when including "Debug" into the 'Condition' derive and 'Instruction' derive.

--- src/types.rs
+++ src/types.rs
@@ -50,14 +50,14 @@ pub fn format_cbits(cbits: &HashMap<usize, i32>, num_cbits: usize) -> String {
         .collect()
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Condition {
     pub creg_base: usize,
     pub creg_size: usize,
     pub creg_value: u64,
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Clone, Debug)]
 #[serde(tag = "kind", rename_all = "snake_case")]
 pub enum Instruction {
     // -----------------------------------------------------------------------
